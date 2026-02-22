f420854d39508c31185b8952d240a0107d1f64e2
CarCor3 <carcela3@gatech.edu>
2026-02-22 17:49:55 -0500

    Contact page opt

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 /*contact page title*/
 export default function Contact() {
     return (
-        <div className="py-32 md:py-60 max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center">
+        <div className="py-36 md:py-60 max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center">
             <h1 className="font-daydream text-5xl md:text-5xl font-bold mb-12 text-center leading-[1.5] md:leading-normal" style={{ color: '#000000' }}>
                 GET IN TOUCH
             </h1>
