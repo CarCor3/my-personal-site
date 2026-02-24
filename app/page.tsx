bef34cc7c7a86f9ecc334cfc256608a6035bd783
CarCor3 <carcela3@gatech.edu>
2026-02-23 21:15:16 -0500

    fixed minor issues for desktop

--- app/page.tsx
+++ app/page.tsx
@@ -269,7 +269,7 @@ export default function Home() {
                 style={{
                     position: 'absolute',
                     top: isMobile ? '20%' : '47%',
-                    left: isMobile ? '50%' : '67%',
+                    left: isMobile ? '49%' : '67%',
                     zIndex: 2,
                     textAlign: 'center',
                     width: isMobile ? '90%' : 'auto',
